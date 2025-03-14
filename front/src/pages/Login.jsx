d6c14bbf15e5e5e508c56f3f1476cee90e75ac53
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2025-03-14 09:07:26 +0300

    update login

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center top-0 -mt-40 h-screen bg-gradient-to-r from-blue-500 to-teal-500">
+    <div className="flex items-center justify-center top-0 -mt-40 h-screen overflow-y-hidden bg-gradient-to-r from-blue-500 to-teal-500">
       <form
         onSubmit={handleSubmit(onSubmit)}
         className={`bg-white p-8 rounded-lg shadow-lg w-96 transition-transform transform ${
@@ -84,7 +84,7 @@ const Login = () => {
         <button
           type="submit"
           disabled={isSubmitting}
-          className={`w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200 ${
+          className={`w-full bg-blue-600 text-white py-2 cursor-pointer rounded-lg hover:bg-blue-700 transition duration-200 ${
             isSubmitting ? "opacity-50 cursor-not-allowed" : ""
           }`}
         >
