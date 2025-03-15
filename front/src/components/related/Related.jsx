54f8d6594c9897845d6901788ddee9ad8ea9ecca
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2025-03-16 02:03:03 +0300

    add functionality for the related data based on the category of the posts

--- front/src/components/related/Related.jsx
+++ front/src/components/related/Related.jsx
@@ -1,36 +1,50 @@
 import React from "react";
-import post1 from "../../assets/post1.jpg";
 import { Link } from "react-router-dom";
+import api from "../../api/api";
+import { useQuery } from "@tanstack/react-query";
 
-const Related = () => {
+const fetchRelatedBlogs = async () => {
+  const response = await api.get("/posts");
+  return response;
+};
+const Related = ({ category, itemId }) => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["related"],
+    queryFn: fetchRelatedBlogs,
+  });
+
+  if (isPending) return <div>Loading ...</div>;
+  if (error) return <div>Error is happening!</div>;
+
+  const relatedBlogs = data?.data?.data?.filter(
+    (item) => item.category === category
+  );
+
+  const filteredBlogs = relatedBlogs.filter((item) => item.id !== itemId);
   return (
     <div>
-      <div>
-        <div className="w-fit max-w-[350px] flex flex-col shadow-2xl shadow-gray-900 border-gray-600 border-[1px] rounded-2xl p-2">
-          <img src={post1} alt="post one" className="h-[300px]  rounded-2xl" />
-          <p className="font-semibold py-2 text-xl md:text-2xl lg:text-3xl text-center">
-            In Ubuntu, you can record your screen
-          </p>
-          <p className="line-clamp-2">
-            or video using various methods. Here are some of the best options:
-            1. Built-in GNOME Screen Recorder (For Ubuntu 22.04 and later) If
-            you're using GNOME (default in Ubuntu), you can use the built-in
-            screen recorder: Steps: Press Ctrl + Alt + Shift + R to start
-            recording. A red dot will appear in the top-right corner, indicating
-            recording is in progress. Press the same shortcut (Ctrl + Alt +
-            Shift + R) again to stop recording. The recorded video is saved in
-            ~/Videos/Screencasts. ⚠️ This built-in recorder only records the
-            entire screen and does not support audio recording.
-          </p>
-          <div className="flex justify-end items-end mt-2">
-            <Link to="">
-              <button className="hover:text-blue-500 underline cursor-pointer">
-                Read More
-              </button>
-            </Link>
+      {filteredBlogs.slice(0, 2).map((item) => (
+        <div key={item.id}>
+          <div className="w-fit max-w-[350px] flex flex-col shadow-2xl shadow-gray-900 border-gray-600 border-[1px] rounded-2xl p-2">
+            <img
+              src={`http://localhost:3000/uploads/${item.image}`}
+              alt="post one"
+              className="h-[300px]  rounded-2xl"
+            />
+            <p className="font-semibold py-2 text-xl md:text-2xl lg:text-3xl text-center">
+              {item.title}
+            </p>
+            <p className="line-clamp-2">{item.description}</p>
+            <div className="flex justify-end items-end mt-2">
+              <Link to={`/posts/${item.id}`}>
+                <button className="hover:text-blue-500 underline cursor-pointer">
+                  Read More
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
